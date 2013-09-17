c26a79cce262906b5e32cc389b196c7fa89e32bf
dEBUGGER <dz@stackstorm.com>
2013-09-17 10:56:47 -0700

    Fixed VM creation on client up. Time Vagrant.

--- up.sh
+++ up.sh
@@ -6,5 +6,9 @@ cp id_rsa_libvirt ~/.ssh/
 cp id_rsa_libvirt.pub ~/.ssh/
 ssh-add ~/.ssh/id_rsa_libvirt
 
-# Trigger vagrant
+# Trigger vagrant (time it)
+source timer.sh 
+t=$(timer)
 vagrant up
+read -p 'Enter when ready...' p
+printf 'Elapsed time: %s\n' $(timer $t)
